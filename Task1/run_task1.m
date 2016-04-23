4d933a068749a72ed9769634964a5561975f43a1
Jan <Jan.kaemper@gmail.com>
2016-04-24 01:24:19 +0200

    10min interval

--- Task1/run_task1.m
+++ Task1/run_task1.m
@@ -12,9 +12,11 @@ t = (datenum(time_stamp, 'yyyy-mm-dd HH:MM:SS'));
 clear time_stamp;
 t = round(t*24*3600);
 disp('Marking invalid data ...')
-raw_data(raw_data==-999) = NaN(size(raw_data(raw_data==-999)));
+%raw_data(raw_data==-999) = NaN(size(raw_data(raw_data==-999)));
+raw_data(raw_data==-999) = NaN;
 disp('Creating continous time axis')
 tnew=[t(1):1:t(end)]';
+n = length(tnew);
 data_pp = NaN(length(tnew),10);
 disp('Writing preprocessed Data...')
 for i = 1:length(raw_data(:,1))
@@ -23,15 +25,19 @@ end
 time = (1:length(data_pp))';
 data_pp = [time, data_pp];
 save('data_pp.mat', 'data_pp', 'raw_data');
-%clear;
+    
 %% Evaluation
-clear;
 load('data_pp.mat')
 
 % means and 10 min means
-means = nanmean(data_pp);
-stds = nanstd(data_pp);
-
+disp('Computing 10min means and stddev')
+means_interval10 = zeros(round(n/600),20);
+for i = 1:n/600
+   for j=1:10  
+       means_interval10(i,j*2-1) = nanmean(data_pp((i-1)*600+1:i*600,j+1));
+       means_interval10(i,j*2) = nanstd(data_pp((i-1)*600+1:i*600,j+1));
+   end
+end
 
 % Wind directions
 u90 = sind(data_pp(:,2));
