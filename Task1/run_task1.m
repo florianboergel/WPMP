a575b35c512b0835700264cb781b3032221382a4
Florian <florian.boergel@uni-oldenburg.de>
2016-04-27 12:04:00 +0200

    ...

--- Task1/run_task1.m
+++ Task1/run_task1.m
@@ -30,7 +30,7 @@ data_pp = [time, data_pp];
 load('data_pp.mat');
 
 % means and 10 min means
-disp('Computing 10min means and stddev');
+disp('Computing 10min means and stddev');  
 means_interval10 = zeros(round(n/600),20);
 for i = 1:n/600
    %treat wind vanes
@@ -74,7 +74,87 @@ plot(means_interval10(start30thJan+1:start30thJan+24*6,5)-means_interval10(start
 xlabel('time in [s]');
 ylabel('windspeed in [m/s]');
 legend('Mean windspeed','Standard Deviation','Location','northwest');
-disp('saving plot to Plots/mean_intervall_withstd.png');
-saveas(gcf,'Plots/mean_intervall_withstd.png');
+disp('saving plot to Plots/mean_interval_withstd.png');
+saveas(gcf,'Plots/mean_interval_withstd.png');
 hold off;
 
+%% spikes
+load('meansAndStddev.mat');
+load('data_pp.mat');
+j= 1;
+k = 1;
+for i = 1:length(data_pp(:,1))/600
+    if max((data_pp((i-1)*600+1:i*600,4))) > (means_interval10(i,5)+5*means_interval10(i,6))
+        spikes(j,1) = i
+        j = j+1;
+    end
+    if min((data_pp((i-1)*600+1:i*600,4))) < (means_interval10(i,5)-5*means_interval10(i,6))
+        spikes(k,2) = i
+        k = k+1;
+    end
+end 
+
+% Plot spikes
+i = 1084;
+window = 1 
+figure();
+for i = spikes(2:5,1)'
+    subplot(2,2,window)
+    plot_data_mean(1:600,1) = means_interval10(i,5);
+    plot_data_mean(1:600,2) = means_interval10(i,5)+5*means_interval10(i,6);
+    plot_data_mean(1:600,3) = means_interval10(i,5)-5*means_interval10(i,6);
+
+    hold on;
+    plot((data_pp((i-1)*600+1:i*600,4)), '-r')
+    plot(plot_data_mean(1:600,1),'-g')
+    plot(plot_data_mean(1:600,2),'-b')
+    plot(plot_data_mean(1:600,3),'-b')
+    xlabel('Time in [s]')
+    ylabel('Windspeed in [m/s]')
+    legend('Windspeed','Mean Windspeed','Standard Deviation', 'Location', 'northwest');
+    hold off;
+    window = window + 1;
+end
+disp('saving plot to Plots/10min_interval_with_highspikes.png')
+saveas(gcf,'Plots/10min_interval_with_highspikes.png')
+
+window = 1 
+figure();
+for i = spikes(1:9,2)'
+    subplot(3,3,window)
+    plot_data_mean(1:600,1) = means_interval10(i,5);
+    plot_data_mean(1:600,2) = means_interval10(i,5)+5*means_interval10(i,6);
+    plot_data_mean(1:600,3) = means_interval10(i,5)-5*means_interval10(i,6);
+
+    hold on;
+    plot((data_pp((i-1)*600+1:i*600,4)), '-r')
+    plot(plot_data_mean(1:600,1),'-g')
+    plot(plot_data_mean(1:600,2),'-b')
+    plot(plot_data_mean(1:600,3),'-b')
+    xlabel('Time in [s]')
+    ylabel('Windspeed in [m/s]')
+    legend('Windspeed','Mean Windspeed','Standard Deviation', 'Location', 'northwest');
+    hold off;
+    window = window + 1;
+end
+disp('saving plot to Plots/10min_interval_with_lowerspikes.png')
+saveas(gcf,'Plots/10min_interval_with_lowerspikes.png')
+%% Task 6
+load('data_pp.mat')
+load('meansAndStddev.mat');
+tau = 1;
+for i = 1:length(data_pp(:,4))-1
+    du(i,1) = data_pp(i+tau,4)-data_pp(i,4);
+end
+
+du = du/nanstd(du);
+[hist_y, hist_x] = hist(du, 100);
+hist_y = hist_y/sum(hist_y);
+gausx = -10:0.1:10;
+gausy = (1/sqrt(2*pi))*exp(-gausx.^2/2);
+
+figure();
+semilogy(hist_x, hist_y);
+hold on;
+semilogy(gausx,gausy);
+hold off;
