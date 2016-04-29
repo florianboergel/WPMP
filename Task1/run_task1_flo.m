25bd3692b67b38e155d50bbfbdbef716ecfb5017
Florian <florian.boergel@uni-oldenburg.de>
2016-04-29 16:43:45 +0200

    neuer code

--- Task1/run_task1_flo.m
+++ Task1/run_task1_flo.m
@@ -2,37 +2,31 @@
 disp('Preprocessing Data')
 disp('Loading Data ...')
 raw_data = readtable('1301.txt','Delimiter','tab');
-disp('Writing timestamp ...')
 time_stamp = raw_data{:, {'Time'}};
-disp('Writing raw_data')
 raw_data = raw_data{:, {'d90', 'd33', 'u100', 'u90', 'u80', ...
 'u70', 'u60', 'u50', 'u40', 'u33'}};
-disp('Converting time with datenum ...')
 t = (datenum(time_stamp, 'yyyy-mm-dd HH:MM:SS'));
 clear time_stamp;
 t = round(t*24*3600);
-disp('Marking invalid data ...')
-%raw_data(raw_data==-999) = NaN(size(raw_data(raw_data==-999)));
 raw_data(raw_data==-999) = NaN;
-disp('Creating continous time axis')
-tnew=[t(1):1:t(end)]';
+last_time = 31*24*3600;
+tnew=[1:1:last_time]';
 n = length(tnew);
 data_pp = NaN(length(tnew),10);
-disp('Writing preprocessed Data...')
 for i = 1:length(raw_data(:,1))
     data_pp(t(i)-t(1)+1, :) = raw_data(i, :);
 end
 time = (1:length(data_pp))';
 data_pp = [time, data_pp];
-    save('data_pp.mat', 'data_pp', 'raw_data');
-
+save('data_pp.mat', 'data_pp', 'raw_data');
+clearvars;
 %% Evaluation
 load('data_pp.mat');
 
 % means and 10 min means
 disp('Computing 10min means and stddev');  
-means_interval10 = zeros(round(n/600),20);
-for i = 1:n/600
+means_interval10 = NaN((length(data_pp(:,1))/600),20);
+for i = 1:length(data_pp(:,1))/600
    %treat wind vanes
    radians = data_pp((i-1)*600+1:i*600,2)/180*pi;
    meanSin = nanmean(sin(radians));
@@ -67,17 +61,18 @@ save('meansAndStddev.mat', 'means_interval10');
 load('meansAndStddev.mat');
 start30thJan = 29*24*6;
 figure();
+print -r1500
 plot(means_interval10(start30thJan+1:start30thJan+24*6,5), '-r');
 hold on;
 plot(means_interval10(start30thJan+1:start30thJan+24*6,5)+means_interval10(start30thJan+1:start30thJan+24*6,6), '--b');
 plot(means_interval10(start30thJan+1:start30thJan+24*6,5)-means_interval10(start30thJan+1:start30thJan+24*6,6), '--b');
-xlabel('time in [s]');
+xlabel('10 minutes interval count [1]');
 ylabel('windspeed in [m/s]');
 legend('Mean windspeed','Standard Deviation','Location','northwest');
 disp('saving plot to Plots/mean_interval_withstd.png');
-saveas(gcf,'Plots/mean_interval_withstd.png');
+print('Plots/mean_interval_withstd.png','-dpng','-r500')
 hold off;
-
+clearvars;
 %% spikes
 load('meansAndStddev.mat');
 load('data_pp.mat');
@@ -98,8 +93,8 @@ end
 i = 1084;
 window = 1 
 figure();
-for i = spikes(2:5,1)'
-    subplot(2,2,window)
+for i = spikes(1:5,1)'
+    figure();
     plot_data_mean(1:600,1) = means_interval10(i,5);
     plot_data_mean(1:600,2) = means_interval10(i,5)+5*means_interval10(i,6);
     plot_data_mean(1:600,3) = means_interval10(i,5)-5*means_interval10(i,6);
@@ -113,15 +108,17 @@ for i = spikes(2:5,1)'
     ylabel('Windspeed in [m/s]')
     legend('Windspeed','Mean Windspeed','Standard Deviation', 'Location', 'northwest');
     hold off;
-    window = window + 1;
+    stri = num2str(i);
+    str = strcat('Plots/spikesintervall', stri, '.png');
+    print(str,'-dpng','-r500');
 end
 disp('saving plot to Plots/10min_interval_with_highspikes.png')
 saveas(gcf,'Plots/10min_interval_with_highspikes.png')
 
 window = 1 
 figure();
-for i = spikes(1:9,2)'
-    subplot(3,3,window)
+for i = spikes(:,2)'
+    figure();
     plot_data_mean(1:600,1) = means_interval10(i,5);
     plot_data_mean(1:600,2) = means_interval10(i,5)+5*means_interval10(i,6);
     plot_data_mean(1:600,3) = means_interval10(i,5)-5*means_interval10(i,6);
@@ -135,10 +132,13 @@ for i = spikes(1:9,2)'
     ylabel('Windspeed in [m/s]')
     legend('Windspeed','Mean Windspeed','Standard Deviation', 'Location', 'northwest');
     hold off;
-    window = window + 1;
+    stri = num2str(i);
+    str = strcat('Plots/spikesintervall', stri, '.png');
+    print(str,'-dpng','-r500');
 end
-disp('saving plot to Plots/10min_interval_with_lowerspikes.png')
-saveas(gcf,'Plots/10min_interval_with_lowerspikes.png')
+%disp('saving plot to Plots/10min_interval_with_lowerspikes.png')
+%saveas(gcf,'Plots/10min_interval_with_lowerspikes.png')
+clearsvars
 %% Task 6
 load('data_pp.mat')
 load('meansAndStddev.mat');
@@ -150,7 +150,7 @@ end
 du = du/nanstd(du);
 [hist_y, hist_x] = hist(du, 100);
 hist_y = hist_y/sum(hist_y);
-gausx = -10:0.1:10;
+gausx = -10:1:10;
 gausy = (1/sqrt(2*pi))*exp(-gausx.^2/2);
 
 figure();
@@ -158,3 +158,5 @@ semilogy(hist_x, hist_y);
 hold on;
 semilogy(gausx,gausy);
 hold off;
+print('Plots/pdfplot','-dpng','-r500')
+clearvars
