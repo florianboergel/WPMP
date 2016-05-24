30516988daeaae303ddeb853d5c8014778558792
Jan <Jan.kaemper@gmail.com>
2016-05-24 13:46:28 +0200

    report refs

--- Task2/task2_main.m
+++ Task2/task2_main.m
@@ -34,14 +34,14 @@ mean2 = nanmean(fino2_v92);
 dev2 = nanstd(fino2_v92);
 
 % interpolate
-k_Fino1 = 1;
+k_Fino1 = 2.073;
 Func_Fino1 = @(k_Fino1) (mean1*mean1/(dev1*dev1))*((gamma(1+2/k_Fino1))/(gamma(1+1/k_Fino1))^2-1)-1 
 k_Fino1 = fsolve(Func_Fino1,k_Fino1);
 disp(k_Fino1);
 A_Fino1 = mean1/gamma(1+1/k_Fino1);
 weibull_Fino1 = wblpdf(1:30,A_Fino1,k_Fino1);
 
-k_Fino2 = 1;
+k_Fino2 = 2.2426;
 Func_Fino2 = @(k_Fino2) (mean2*mean2/(dev2*dev2))*((gamma(1+2/k_Fino2))/(gamma(1+1/k_Fino2))^2-1)-1 
 k_Fino2 = fsolve(Func_Fino2,k_Fino2);
 disp(k_Fino2);
@@ -139,23 +139,121 @@ for i=1:8
 end
 figure();
 hold on;
-plot([33,40,50,60,70,80,90,100],avgPerHeight(:), 'o')
+plot(avgPerHeight(:),[33,40,50,60,70,80,90,100], 'o')
 
 logProfileModel = @(b,z) b(1)/b(2) *(log(z/b(3)));
 empPowerModel = @(c,x) avgPerHeight(8)*((x/90).^c(1));
 opts = statset('nlinfit');
 opts.RobustWgtFun = 'bisquare';
 logProfileCoeffs = nlinfit([33,40,50,60,70,80,90,100],avgPerHeight,logProfileModel,[0.2,0.4,10^-6],opts);
-fplot(@(z) logProfileCoeffs(1)/logProfileCoeffs(2) *(log(z/logProfileCoeffs(3))));
+[x,y]=fplot(@(z) logProfileCoeffs(1)/logProfileCoeffs(2) *(log(z/logProfileCoeffs(3))),[0 100]);
+plot(y,x,'Color','b');
 empPowerCoeff = nlinfit([33,40,50,60,70,80,90,100],avgPerHeight,empPowerModel,[0.11],opts);
-fplot(@(z) avgPerHeight(8)*(z/90)^(empPowerCoeff));
+[x,y]=fplot(@(z) avgPerHeight(8)*(z/90)^(empPowerCoeff),[0 100]);
+plot(y,x,'Color','r');
 
-xlabel('Height in [m]');
-ylabel('windspeed in [m/s]');
+ylabel('Height in [m]');
+xlabel('windspeed in [m/s]');
 title('Vertical Profile All Heights');
-legend('Data', 'Log Profile','Empirical Power Profile');
+legend('Data', 'Log Profile','Empirical Power Profile','Location','northwest');
 saveas(gcf,'figures/verticalProfileFits.png')
 hold off;
 
 %% ExtraTask 3(b)
 
+windsOnHeightSommer = [];
+windsOnHeightWinter = [];
+jS = 1
+jW = 1
+
+for i = 1:length(fino1_v90)
+    if (fino1_d90(i) >= 240 && fino1_d90(i) <= 285) 
+        if  ((i>6*24*(31+28+31+30) && i<=6*24*(31+28+31+30+31+30+31)) ... %May-July 2010
+            || (i>6*24*(31+28+31+30+365) && i<=6*24*(31+28+31+30+31+30+31+365)) ... %May-July 2011
+            || (i>6*24*(31+29+31+30+365+365) && i<=6*24*(31+29+31+30+31+30+31+365+365)) ... %May-July 2012
+            || (i>6*24*(31+28+31+30+365+365+366) && i<=6*24*(31+28+31+30+31+30+31+365+365+366)) ... %May-July 2013
+            || (i>6*24*(31+28+31+30+365+365+366+365) && i<=6*24*(31+28+31+30+31+30+31+365+365+366+365)) ... %May-July 2014
+            || (i>6*24*(31+28+31+30+365+365+366+365+365) && i<=6*24*(31+28+31+30+31+30+31+365+365+366+365+365))) %May-July 2015
+                windsOnHeightSommer(jS,1) = Fino1.ws33(1,i);
+                windsOnHeightSommer(jS,2) = Fino1.ws40(1,i);
+                windsOnHeightSommer(jS,3) = Fino1.ws50(1,i);
+                windsOnHeightSommer(jS,4) = Fino1.ws60(1,i);
+                windsOnHeightSommer(jS,5) = Fino1.ws70(1,i);
+                windsOnHeightSommer(jS,6) = Fino1.ws80(1,i);
+                windsOnHeightSommer(jS,7) = Fino1.ws90(1,i);
+                windsOnHeightSommer(jS,8) = Fino1.ws100(1,i);
+                jS= jS+1;
+        end
+         if ((i>0 && i<=6*24*31) ... %January 2010
+            || (i>6*24*(365-31-30) && i<=6*24*(365+31)) ... %Nov2010-Jan2011
+            || (i>6*24*(365+365-31-30) && i<=6*24*(365+365+31)) ... %Nov2011-Jan2012
+            || (i>6*24*(365+365+366-31-30) && i<=6*24*(365+365+366+31)) ... %Nov2012-Jan2013
+            || (i>6*24*(365+365+366+365-31-30) && i<=6*24*(365+365+366+365+31)) ... %Nov2013-Jan2014
+            || (i>6*24*(365+365+366+365+365-31-30) && i<=6*24*(365+365+366+365+365+31)) ... %Nov2014-Jan2015
+            || (i>6*24*(365+365+366+365+365+365-31-30) && i<=6*24*(365+365+366+365+365+365)+1)) %Nov2015-Dez2015
+                windsOnHeightWinter(jW,1) = Fino1.ws33(1,i);
+                windsOnHeightWinter(jW,2) = Fino1.ws40(1,i);
+                windsOnHeightWinter(jW,3) = Fino1.ws50(1,i);
+                windsOnHeightWinter(jW,4) = Fino1.ws60(1,i);
+                windsOnHeightWinter(jW,5) = Fino1.ws70(1,i);
+                windsOnHeightWinter(jW,6) = Fino1.ws80(1,i);
+                windsOnHeightWinter(jW,7) = Fino1.ws90(1,i);
+                windsOnHeightWinter(jW,8) = Fino1.ws100(1,i);
+                jW= jW+1;
+        end
+    end
+end
+
+%Evaluation of Sommer Months 2010-2015
+avgPerHeightSommer = [8];
+for i=1:8
+   avgPerHeightSommer(i) = nanmean(windsOnHeightSommer(:,i));
+end
+figure();
+hold on;
+plot(avgPerHeightSommer(:),[33,40,50,60,70,80,90,100], 'o')
+
+logProfileModel = @(b,z) b(1)/b(2) *(log(z/b(3)));
+empPowerModel = @(c,x) avgPerHeightSommer(8)*((x/90).^c(1));
+opts = statset('nlinfit');
+opts.RobustWgtFun = 'bisquare';
+logProfileCoeffsSom = nlinfit([33,40,50,60,70,80,90,100],avgPerHeightSommer,logProfileModel,[0.2,0.4,10^-6],opts);
+[x,y]=fplot(@(z) logProfileCoeffsSom(1)/logProfileCoeffsSom(2) *(log(z/logProfileCoeffsSom(3))),[0 100]);
+plot(y,x,'Color','b');
+empPowerCoeffSom = nlinfit([33,40,50,60,70,80,90,100],avgPerHeightSommer,empPowerModel,[0.11],opts);
+[x,y]=fplot(@(z) avgPerHeightSommer(8)*(z/90)^(empPowerCoeffSom),[0 100]);
+plot(y,x,'Color','r');
+
+ylabel('Height in [m]');
+xlabel('windspeed in [m/s]');
+title('Vertical Profile May-July');
+legend('Data', 'Log Profile','Empirical Power Profile','Location','northwest');
+saveas(gcf,'figures/verticalProfileFitsSommer.png')
+hold off;
+
+%Evaluation of Winter Months 2010-2015
+avgPerHeightWinter = [8];
+for i=1:8
+   avgPerHeightWinter(i) = nanmean(windsOnHeightWinter(:,i));
+end
+figure();
+hold on;
+plot(avgPerHeightWinter(:),[33,40,50,60,70,80,90,100], 'o')
+
+logProfileModel = @(b,z) b(1)/b(2) *(log(z/b(3)));
+empPowerModel = @(c,x) avgPerHeightWinter(8)*((x/90).^c(1));
+opts = statset('nlinfit');
+opts.RobustWgtFun = 'bisquare';
+logProfileCoeffsWin = nlinfit([33,40,50,60,70,80,90,100],avgPerHeightWinter,logProfileModel,[0.1,0.4,10^-5],opts);
+[x,y]=fplot(@(z) real(logProfileCoeffsWin(1))/real(logProfileCoeffsWin(2)) *(log(z/real(logProfileCoeffsWin(3)))),[0 100]);
+plot(y,x,'Color','b');
+empPowerCoeffWin = nlinfit([33,40,50,60,70,80,90,100],avgPerHeightWinter,empPowerModel,0.11,opts);
+[x,y]=fplot(@(z) avgPerHeightWinter(8)*(z/90)^(empPowerCoeffWin),[0 100]);
+plot(y,x,'Color','r');
+
+ylabel('Height in [m]');
+xlabel('windspeed in [m/s]');
+title('Vertical Profile November-January');
+legend('Data', 'Log Profile','Empirical Power Profile','Location','northwest');
+saveas(gcf,'figures/verticalProfileFitsWinter.png')
+hold off;
